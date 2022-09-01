b4479df29b2a0ec8df4bd551adcf2906cb449a38
U.Q Sayyedmuhammadi <aykam-co@hotmail.com>
2022-09-01 17:12:17 +0430

    reset project data

--- Session 3/script/vendor.js
+++
@@ -1,6 +0,0 @@
-const heading = document.getElementById('myHeading')
-const myBall = document.getElementById('ball')
-heading.innerHTML = 'SALAM DONYA'
-const steps = 50
-let defaultXLocation = 0
-let defaultYLocation = 0
