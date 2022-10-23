7a979b5492b06d1a5fe450176b00f737b921b2ad
UsherQelich SayyedMuhammadi <aykam-co@hotmail.com>
2022-10-23 17:54:19 +0330

    push

--- Examples/to do list 1/app.js
+++ Examples/to do list 1/app.js
@@ -2,6 +2,9 @@ let list = [];
 const title = document.getElementById('title');
 const dueDate = document.getElementById('dueDate');
 const display = document.getElementById('display');
+const saveBtn = document.getElementById("saveBtn");
+const addBtn = document.getElementById("Add");
+let selectedTask;
 function initialize () {
     if (localStorage.getItem('list') != null) {
         list = JSON.parse(localStorage.getItem('list'))
@@ -26,9 +29,24 @@ function addToList () {
     title.focus()
 
 }
+function editTask (index) {
+    selectedTask = index
+    addBtn.style.display = 'none';
+    saveBtn.style.display = 'inline';
+    title.value = list[index].kertop
+    dueDate.value = list[index].dueDate
+}
 function show() {
     let text = '';
     list.forEach((item, index) => (text += `<li>${item.kertop} due date: ${item.dueDate} 
-    <button onclick="list.splice(${index},1);show(); localStorage.setItem('list', JSON.stringify(list)) " >❌</button><button>✏️</button></li>`));
+    <button onclick="list.splice(${index},1);show(); localStorage.setItem('list', JSON.stringify(list)) " >❌</button><button onclick="editTask(${index})">✏️</button></li>`));
     display.innerHTML=text
+}
+function update () {
+    addBtn.style.display = "inline";
+    saveBtn.style.display = "none";
+    list[selectedTask].kertop = title.value;
+    list[selectedTask].dueDate = dueDate.value;
+    localStorage.setItem("list", JSON.stringify(list));
+    show();
 }
